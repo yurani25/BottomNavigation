bba230c4ce67ffd7387d49c99afad738043ca409
alejandro <leidyjohanasantacruzdaza03@gmail.com>
2023-09-19 17:01:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadFragment(HomeFragment())
+        var bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(ChatFragment())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(SettingsFragment())
+                    true
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
+}
+
+
